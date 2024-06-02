47408ec0bdcc46625013f8f2b5975fa53ab62259
iamtoricool <iamtoricool@gmail.com>
2024-06-03 02:32:46 +0600

    updated user routes

--- src/routes/user/user_route.ts
+++ src/routes/user/user_route.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 import { dbClient } from "../../database/database";
+import { eq, lt, gte, ne } from "drizzle-orm";
+
 import { responseWithData } from "../../utils/base_http_response";
+
+import * as schema from "../../database/schema/schema";
 
 export const userRoute = new Hono();
 
@@ -8,4 +12,23 @@ userRoute.get("/", async (ctx) => {
   const users = await dbClient.query.user.findMany();
 
   return ctx.json(responseWithData<any>({ data: users }));
+});
+
+userRoute.get(`/:id`, async (ctx) => {
+  const userId = ctx.req.param()["id"];
+
+  let user;
+  try {
+    user = await dbClient.select().from(schema.user);
+  } catch (error) {
+    console.error(error);
+
+    return ctx.json(
+      responseWithData<typeof error>({
+        data: error,
+      })
+    );
+  }
+
+  return ctx.json(responseWithData<typeof user>({ data: user }));
 });
