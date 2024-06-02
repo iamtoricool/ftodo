25965d14ac92b286a3f8d5f550c7c14259bc4a2f
iamtoricool <iamtoricool@gmail.com>
2024-06-02 23:35:05 +0600

    column renamed

---
+++ src/database/migrations/0001_isCompleted_renamed_is_completed.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "todos" RENAME COLUMN "isCompleted" TO "is_completed";
--- src/database/schema/todo_schema.ts
+++ src/database/schema/todo_schema.ts
@@ -1,8 +1,13 @@
 import { pgTable, uuid, varchar, text, boolean } from "drizzle-orm/pg-core";
 
-export const todosTable = pgTable("todos", {
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
+
+export const todo = pgTable("todos", {
   id: uuid("id").primaryKey().defaultRandom().unique(),
   title: varchar("title", { length: 255 }).notNull(),
   description: text("description"),
-  isCompleted: boolean("isCompleted").default(false),
+  isCompleted: boolean("is_completed").default(false),
 });
+
+const createTodoSchema = createInsertSchema(todo);
