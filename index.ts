a1101c1ccb446b015d6b511c033ef2aea8708984
iamtoricool <iamtoricool@gmail.com>
2024-06-04 19:58:36 +0600

    updated middleware order

--- index.ts
+++ index.ts
@@ -6,16 +6,20 @@ import { testMiddleware } from "./src/middlewares/test_middleware";
 
 import { todoRoute } from "./src/routes/todo/todo_route";
 import { userRoute } from "./src/routes/user/user_route";
+import { authRoute } from "./src/routes/auth/auth_route";
 
 const app = new Hono();
 
-// Middlewares
 app.use(trimTrailingSlash());
 app.use(logger());
 
-app.use("/api/*", testMiddleware);
 
-// Routes
+
+// Auth Route
+app.route("/api/auth", authRoute);
+
+// Auth Protected Routes
+app.use("/api/*", testMiddleware);
 app.route("/api/todos", todoRoute);
 app.route("/api/users", userRoute);
 
